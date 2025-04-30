db7b5bfffd5e71c53dc9a2643ce2734493d64053
Justin Nguyen <122576230+juicebox013@users.noreply.github.com>
2025-04-30 07:26:32 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -9,7 +9,7 @@ function sumValues(num1, num2, add) {
     if (add) {
         let result = 0;
 
-        result = num1 + num2;
+        result = Number(num1) + Number(num2);
 
         return result;
     }
